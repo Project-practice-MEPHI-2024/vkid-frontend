61beab0d96fd3ae8bdc303f3dd710531986216c8
deniskockin <den.kochkin36@mail.ru>
2024-11-25 19:55:06 +0300

    Added avataf for Igor

--- src/app/pages/Home/ui/Home.tsx
+++ src/app/pages/Home/ui/Home.tsx
@@ -1,4 +1,6 @@
 import {Box, Container} from "@chakra-ui/react"
+import {Avatar, AvatarGroup} from "@/app/shared/Avatar/ui/avatar";
+
 
 const Home = () => {
   return (
@@ -6,6 +8,9 @@ const Home = () => {
       <Container>
         здесь будет контент
       </Container>
+      <AvatarGroup>
+        <Avatar />
+      </AvatarGroup>
     </Box>
   );
 }
